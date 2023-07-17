b5c82baeb3cb846610c59aa175d5557cd5b8272d
Kurdiansyah Lesmana <kurdilesmana@gmail.com>
2023-07-17 08:01:53 +0700

    remove unused file

--- resources/views/pages/layanan/list/_table.blade.php
+++
@@ -1,8 +0,0 @@
-<!--begin::Table-->
-{{ $dataTable->table(['class' => 'table table-row-bordered gy-5']) }}
-<!--end::Table-->
-
-{{-- Inject Scripts --}}
-@section('scripts')
-    {{ $dataTable->scripts() }}
-@endsection
